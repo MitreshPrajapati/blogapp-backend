df69a41946d73aca52306d5a1712fe2b2bd81dec
MitreshPrajapati <104007923+MitreshPrajapati@users.noreply.github.com>
2023-02-10 21:35:57 +0530

    updated

--- Controller/BlogPostController.js
+++ Controller/BlogPostController.js
@@ -15,7 +15,7 @@ const getPosts = async (req, res) => {
 
 // create new post 
 const createPost = async (req, res) => {
-    const { userId, username } = req.body;
+    const { userId, user_name } = req.body;
     console.log(req.body);
     // req.body.images = "https://www.netscribes.com/wp-content/uploads/2019/06/Technology-Watch.jpg";
     const new_Post = new BlogPost(req.body)
